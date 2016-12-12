8e8367ac261da8690908d55da5b1972cded6013d
aliosa113 <aliosa113@gmail.com>
2016-12-12 14:03:30 +0700

    Sửa lại loginservlet

--- src/java/com/javaweb/controller/LoginServlet.java
+++ src/java/com/javaweb/controller/LoginServlet.java
@@ -11,7 +11,6 @@ import com.javaweb.service.EnDeCryption;
 import com.javaweb.service.NguoidungService;
 import com.javaweb.service.TaikhoanService;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.security.InvalidKeyException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -38,7 +37,7 @@ public class LoginServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException, InvalidKeyException {
         response.setContentType("text/html;charset=UTF-8");
-        
+
         String username, password;
         username = request.getParameter("username");
         password = request.getParameter("pwd");
@@ -49,32 +48,40 @@ public class LoginServlet extends HttpServlet {
         TaikhoanService tk = new TaikhoanService();
         NguoidungService ndsv = new NguoidungService();
         KiemTra = tk.CheckLogin(username, passmahoa);
-        
-        HttpSession session = request.getSession();
 
+        HttpSession session = request.getSession();
+        
         if (KiemTra) {
             Taikhoan taikhoan = tk.GetUserByEmailOrUserName(username);
             Nguoidung nguoidung = ndsv.GetUserByIdTaikhoan(taikhoan.getIdTaiKhoan());
             session.setAttribute("youruser", nguoidung.getHo() + " " + nguoidung.getDemVaTen());
-            String url = "/index.jsp";
-            getServletContext().getRequestDispatcher(url).forward(request, response);
+            try {
+                Thread.sleep(1000);
+                String url = "/index.jsp";
+                getServletContext().getRequestDispatcher(url).forward(request, response);
+            } catch (InterruptedException e) {
+            }
         } else {
-            String url = "/Login.jsp";
-            getServletContext().getRequestDispatcher(url).forward(request, response);
+            try {
+                Thread.sleep(1000);
+                String url = "/index.jsp";
+                getServletContext().getRequestDispatcher(url).forward(request, response);
+            } catch (InterruptedException e) {
+            }
         }
-        
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet LoginServlet</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+
+//        try (PrintWriter out = response.getWriter()) {
+//            /* TODO output your page here. You may use following sample code. */
+//            out.println("<!DOCTYPE html>");
+//            out.println("<html>");
+//            out.println("<head>");
+//            out.println("<title>Servlet LoginServlet</title>");
+//            out.println("</head>");
+//            out.println("<body>");
+//            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
+//            out.println("</body>");
+//            out.println("</html>");
+//        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
