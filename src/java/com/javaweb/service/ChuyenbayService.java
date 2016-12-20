54511d8ebbd9e46ee84c06780ed8c5a0e5d0681a
aliosa113 <aliosa113@gmail.com>
2016-12-20 09:12:55 +0700

    Fix ChuyenbayService

--- src/java/com/javaweb/service/ChuyenbayService.java
+++ src/java/com/javaweb/service/ChuyenbayService.java
@@ -17,13 +17,13 @@ import org.hibernate.Transaction;
  * @author DuongNguyen
  */
 public class ChuyenbayService {
-
-    //Tìm chuyến bay
-    public ArrayList<Chuyenbay> SearchChuyenbay(String Tu, String Den, String NgayKhoiHanh) {
+    
+    
+    //Tìm kiếm chuyến bay theo yêu cầu khách hàng
+    public ArrayList<Chuyenbay>TimKiemChuyenbay(String Tu, String Den, String NgayKhoiHanh) {
         Session session = HibernateUtil.getSessionFactory().openSession();
         Transaction tx = null;
         ArrayList listChuyenbay = new ArrayList<Chuyenbay>();
-
         try {
             tx = session.getTransaction();
             tx.begin();
@@ -33,7 +33,6 @@ public class ChuyenbayService {
                     + "and NgayKhoiHanh = '" + NgayKhoiHanh + "'");
             listChuyenbay = (ArrayList) query.list();
             tx.commit();
-
         } catch (Exception e) {
             if (tx != null) {
                 tx.rollback();
@@ -68,23 +67,53 @@ public class ChuyenbayService {
         return null;
     }
 
+    public int chuyenbaycount = 0;
+
     //Lấy tất cả thông tin tài khoản
-    public ArrayList<Chuyenbay> GetAllChuyenbay() {
+    public ArrayList<Chuyenbay> GetAllChuyenbay(int pageSize, int pageNumber) {
         Session session = HibernateUtil.getSessionFactory().openSession();
         Transaction tx = null;
         ArrayList<Chuyenbay> listChuyenbay = new ArrayList<Chuyenbay>();
         try {
             tx = session.getTransaction();
             tx.begin();
-
             Query query = session.createQuery("from Chuyenbay");
+            chuyenbaycount = query.list().size();
+            query = query.setFirstResult(pageSize * (pageNumber - 1));
+            query.setMaxResults(pageSize);
             listChuyenbay = (ArrayList) query.list();
             tx.commit();
         } catch (Exception e) {
             if (tx != null) {
                 tx.rollback();
             }
-            System.out.println("Loi" + e.toString());
+        } finally {
+            session.close();
+        }
+        return listChuyenbay;
+    }
+
+    //Tìm chuyến bay
+    public ArrayList<Chuyenbay>SearchChuyenbay(int pageSize, int pageNumber, String Tu, String Den, String NgayKhoiHanh) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+        ArrayList listChuyenbay = new ArrayList<Chuyenbay>();
+        try {
+            tx = session.getTransaction();
+            tx.begin();
+            Query query = session.createQuery("from Chuyenbay where "
+                    + "Tu like  '" + Tu + "' and "
+                    + "Den like '" + Den + "' "
+                    + "and NgayKhoiHanh = '" + NgayKhoiHanh + "'");
+            chuyenbaycount = query.list().size();
+            query = query.setFirstResult(pageSize * (pageNumber - 1));
+            query.setMaxResults(pageSize);
+            listChuyenbay = (ArrayList) query.list();
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
         } finally {
             session.close();
         }
