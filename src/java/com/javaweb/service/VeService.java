de6c5782713dd129800f855fdb223776233edaf7
aliosa113 <aliosa113@gmail.com>
2017-01-02 23:38:35 +0700

    Service fix

--- src/java/com/javaweb/service/VeService.java
+++ src/java/com/javaweb/service/VeService.java
@@ -7,6 +7,7 @@ package com.javaweb.service;
 
 import com.javaweb.hibernate.util.HibernateUtil;
 import com.javaweb.model.Ve;
+import java.util.ArrayList;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -61,5 +62,74 @@ public class VeService {
             session.close();
         }
         return null;
+    }
+
+    //Lấy thông tin vé theo ID Taikhoan
+    public ArrayList<Ve> GetVeByIdTaikhoan(String VeID) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+        ArrayList<Ve> listVe = new ArrayList<Ve>();
+        try {
+            tx = session.getTransaction();
+            tx.begin();
+            Query query = session.createQuery("from Ve where idtaikhoan = " + VeID);
+            listVe = (ArrayList) query.list();
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            System.out.println("Error!" + e.toString());
+        } finally {
+            session.close();
+        }
+        return listVe;
+    }
+    
+    //Lấy tất cả thông tin Vé
+    public ArrayList<Ve> GetAllVe() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+        ArrayList<Ve> listVe = new ArrayList<Ve>();
+        try {
+            tx = session.getTransaction();
+            tx.begin();
+            Query query = session.createQuery("from Ve");
+            listVe = (ArrayList) query.list();
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            System.out.println("Loi" + e.toString());
+        } finally {
+            session.close();
+        }
+        return listVe;
+    }
+    
+    public int vecount = 0;
+    //Lấy tất cả thông tin vé chia page
+    public ArrayList<Ve> GetAllVe(int pageSize, int pageNumber) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+        ArrayList<Ve> listVe = new ArrayList<Ve>();
+        try {
+            tx = session.getTransaction();
+            tx.begin();
+            Query query = session.createQuery("from Ve");
+            vecount = query.list().size();
+            query = query.setFirstResult(pageSize * (pageNumber - 1));
+            query.setMaxResults(pageSize);
+            listVe = (ArrayList) query.list();
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+        } finally {
+            session.close();
+        }
+        return listVe;
     }
 }
